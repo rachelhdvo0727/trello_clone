3153e639327078d8fd3854b30bf6b9b616278f68
rachelvo <rach0727@stud.kea.dk>
2020-04-22 20:35:16 +0200

    reinstall packages

---
+++ jsmodules/vars.js
@@ -0,0 +1,11 @@
+export const apiKey = "5e956ffd436377171a0c230f";
+export const dataTrello =
+  "https://frontendspring20-e4cd.restdb.io/rest/trelloclone";
+export const addNewBtn = document.querySelector(".addNew");
+export const form = document.querySelector("form");
+export const displayList = document.querySelector("#todoList > .list");
+export const elements = form.elements;
+export const formElms = form.querySelectorAll("input");
+export const errormsgs = document.querySelectorAll(".msg");
+export const cards = document.querySelectorAll("#cardcontainer");
+export let validForm = true;
